7798a598e441e0127a323bc9bbd56b029d09308e
Vi Vo <vptv1310@gmail.com>
2023-12-09 23:03:00 +0700

    Update code to stop calling service

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -5,7 +5,12 @@ export default function Room({ folderName, roomName }) {
   return (
     <>
       <Typography variant="h4">{roomName}</Typography>
-      <ImageGallery folderName={folderName} />
+      <Typography variant="h6">
+        Aimer.dalat has stopped operating. Thanks for all of your love and
+        support thus far!
+      </Typography>
+      {/* Temporarily suspend to avoid billing */}
+      {/* <ImageGallery folderName={folderName} /> */}
     </>
   );
 }
